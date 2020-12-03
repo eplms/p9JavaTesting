340838fc1fc902fef24ba9136f98b9846e59b2e1
Emmanuel Plumas <eplumas@free.fr>
2020-12-03 16:12:02 +0100

    Implémentation du second Test d'intégration sur addReference
    Test sur la valeur réelle de la référence

--- src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteIntegrationTest.java
+++ src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteIntegrationTest.java
@@ -7,6 +7,7 @@ import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
 import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.math.BigDecimal;
@@ -41,7 +42,27 @@ public class ComptabiliteIntegrationTest extends BusinessTestCase {
 		// Vérification que la référence a bien été ajoutée à l'écriture comptable
 		assertNotNull(vEcritureComptable.getReference());
 	}
-	
+	@Test
+	public void addReferenceTestShouldSetReferenceToEcritureComptable() {
+		// ARRANGE : Initialisation d'une écriture comptable sans référence
+		EcritureComptable vEcritureComptable = new EcritureComptable();
+		vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
+		vEcritureComptable.setDate(new Date());
+		vEcritureComptable.setReference(null);
+		vEcritureComptable.setLibelle("Libelle");
+		vEcritureComptable.getListLigneEcriture()
+				.add(new LigneEcritureComptable(new CompteComptable(1), null, new BigDecimal(123), null));
+		vEcritureComptable.getListLigneEcriture()
+				.add(new LigneEcritureComptable(new CompteComptable(2), null, null, new BigDecimal(123)));
+
+		// ACT
+		// L'écriture comptable est passée dans la méthode addReference
+		getBusinessProxy().getComptabiliteManager().addReference(vEcritureComptable);
+		// reference est "AC-2020/xxxxx"
+		// ASSERT
+		// Vérification que la référence a bien été ajoutée à l'écriture comptable
+		assertEquals(vEcritureComptable.getReference(),"AC-2020/00002");
+	}
 	
 	
 	
