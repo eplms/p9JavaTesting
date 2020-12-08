d9548132e8e27f530146282f65899743c3294619
Emmanuel Plumas <eplumas@free.fr>
2020-12-08 16:17:34 +0100

    Implémentation Test Integration checkEcritureComptable

--- src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteIntegrationTest.java
+++ src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteIntegrationTest.java
@@ -3,10 +3,13 @@ package com.dummy.myerp.testbusiness.business;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 
+import com.dummy.myerp.business.impl.manager.ComptabiliteManagerImpl;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
 import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
+import com.dummy.myerp.technical.exception.FunctionalException;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -17,7 +20,10 @@ import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ComptabiliteIntegrationTest extends BusinessTestCase {
+	
+	private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
 
+	
 	@Test
 	public void test1() {
 		assertNotNull(getBusinessProxy().getComptabiliteManager().getListCompteComptable());
@@ -79,5 +85,39 @@ public class ComptabiliteIntegrationTest extends BusinessTestCase {
 		// ASSERT : Vérification que la référence a bien été ajoutée à l'écriture comptable
 		assertEquals("00002",vEcritureComptable.getReference().substring(8));
 	}
-
+	
+	
+	@Test (expected =FunctionalException.class)
+	public void checkEcritureComptableShouldThrowExceptionWhenReferenceEcritureComptableAlreadyExist() throws Exception {
+		//ARRANGE : Initialisation d'une écriture existante en base à la création de la base
+		EcritureComptable vEcritureComptable = new EcritureComptable();
+		vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));		
+		vEcritureComptable.setDate(new Date());
+		vEcritureComptable.setReference("AC-2016/00001");
+		vEcritureComptable.setLibelle("Libelle");
+		vEcritureComptable.getListLigneEcriture()
+				.add(new LigneEcritureComptable(new CompteComptable(1), null, new BigDecimal(123), null));
+		vEcritureComptable.getListLigneEcriture()
+				.add(new LigneEcritureComptable(new CompteComptable(2), null, null, new BigDecimal(123)));
+		//ACT 
+		manager.checkEcritureComptable(vEcritureComptable);
+		
+	}
+	
+	@Test
+	public void checkEcritureComptableShouldNotThrowExceptionWhenReferenceEcritureComptableDoesntAlreadyExist() throws Exception {
+		//ARRANGE : Initialisation d'une écriture non existante
+		EcritureComptable vEcritureComptable = new EcritureComptable();
+		vEcritureComptable.setJournal(new JournalComptable("VE", "Vente"));		
+		vEcritureComptable.setDate(new Date());
+		getBusinessProxy().getComptabiliteManager().addReference(vEcritureComptable);
+		vEcritureComptable.setLibelle("Presta3");
+		vEcritureComptable.getListLigneEcriture()
+				.add(new LigneEcritureComptable(new CompteComptable(1), null, new BigDecimal(123), null));
+		vEcritureComptable.getListLigneEcriture()
+				.add(new LigneEcritureComptable(new CompteComptable(2), null, null, new BigDecimal(123)));
+		//ACT 
+		manager.checkEcritureComptable(vEcritureComptable);
+	}
+	
 }
