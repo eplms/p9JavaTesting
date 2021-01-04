20974a7dc4b8fdbc11b1cb61b983dd730f0523e4
Emmanuel Plumas <eplumas@free.fr>
2021-01-04 01:09:34 +0100

    Mise à jour test checkEcritureComptableUnit 2021

--- myerp/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ myerp/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -19,7 +19,7 @@ public class ComptabiliteManagerImplTest {
 		EcritureComptable vEcritureComptable;
 		vEcritureComptable = new EcritureComptable();
 		vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
-		vEcritureComptable.setReference("AC-2020/00001");
+		vEcritureComptable.setReference("AC-2021/00001");
 		vEcritureComptable.setDate(new Date());
 		vEcritureComptable.setLibelle("Libelle");
 		vEcritureComptable.getListLigneEcriture()
